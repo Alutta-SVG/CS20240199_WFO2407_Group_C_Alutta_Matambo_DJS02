0c4d26124fca86328bae65c8d910049c15e15cdb
Alutta <Aluttamatambo5@gmail.com>
2024-10-27 09:35:14 +0200

    amended logical errors and added converion accerleration

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
+// acceleration conversion 
+const accInKmPerHourSquared = acc * 12.96;
 
 const d2 = d + (vel * (time / 3600)) //calcultes new distance
 
@@ -20,19 +22,17 @@ const d2 = d + (vel * (time / 3600)) //calcultes new distance
 const rf = Math.max(fuel- (fbr * time), 0)//calculates remaining fuel
 
 
-const vel2 = calcNewVel ({acc, vel, time}) => {
+const calcNewVel = ({ vel, acc, time }) => {
   //ensures valid inputs and values 
-  if (typeof vel !== 'number' ||  typeof acc !== 'number' || typeof time !== 'number') {
-    throw new Error('Invalid input type')
-    };
+  if (typeof vel !== 'number' || typeof acc !== 'number' || typeof time !== 'number') {
+    throw new Error('Invalid input values.');
+  };
 
   return vel + (acc * (time / 3600));
 } //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-const vel2 = calcNewVel ({ velocity:vel, acceleration: accInKmPerHourSquared, time});
-  return vel + (acc*time)
-}
+const vel2 = calcNewVel({ velocity: vel, acceleration: accInKmPerHourSquared, time });
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
